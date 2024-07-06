14d43090105763e570fae9ef3519c9de7a507814
Danh Thuong <hachimansuvivel@gmail.com>
2024-07-06 14:48:01 +0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  console.log(input);
   return (
-    <div className="App">
-      app
+    <div className="h-screen flex justify-center border border-red-500 items-center">
+      <input type="text" placeholder="text" className="outline-none border border-blue-600 px-4 py-2 w-[400px]" value={input}
+      onChange={e => setInput(e.target.value)} />
+      <button className="bg-blue-500 text-white px-4 py-2 ml-2 rounded-md">Add</button>
     </div>
   );
 }
